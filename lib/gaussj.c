322abd2e3da2138f69b4682e3d54f2e8425e01f3
Juliusz Gonera <juliusz.gonera@gmail.com>
2013-10-20 15:01:22 +0200

    Fix b vector matrix indexing in gaussj. What were they thinking?

--- lib/gaussj.c
+++ lib/gaussj.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdio.h>
 #define NRANSI
 #include "nrutil.h"
 #define SWAP(a,b) {temp=(a);(a)=(b);(b)=temp;}
@@ -12,6 +13,8 @@ void gaussj(double **a, int n, double **b, int m)
 	indxc=ivector(1,n);
 	indxr=ivector(1,n);
 	ipiv=ivector(1,n);
+
+
 	for (j=1;j<=n;j++) ipiv[j]=0;
 	for (i=1;i<=n;i++) {
 		big=0.0;
@@ -28,8 +31,8 @@ void gaussj(double **a, int n, double **b, int m)
 				}
 		++(ipiv[icol]);
 		if (irow != icol) {
-			for (l=1;l<=n;l++) SWAP(a[irow][l],a[icol][l])
-			for (l=1;l<=m;l++) SWAP(b[irow][l],b[icol][l])
+          for (l=1;l<=n;l++) SWAP(a[irow][l],a[icol][l]);
+          for (l=1;l<=m;l++) SWAP(b[l][irow],b[l][icol]);
 		}
 		indxr[i]=irow;
 		indxc[i]=icol;
@@ -37,13 +40,13 @@ void gaussj(double **a, int n, double **b, int m)
 		pivinv=1.0/a[icol][icol];
 		a[icol][icol]=1.0;
 		for (l=1;l<=n;l++) a[icol][l] *= pivinv;
-		for (l=1;l<=m;l++) b[icol][l] *= pivinv;
+		for (l=1;l<=m;l++) b[l][icol] *= pivinv;
 		for (ll=1;ll<=n;ll++)
 			if (ll != icol) {
 				dum=a[ll][icol];
 				a[ll][icol]=0.0;
 				for (l=1;l<=n;l++) a[ll][l] -= a[icol][l]*dum;
-				for (l=1;l<=m;l++) b[ll][l] -= b[icol][l]*dum;
+				for (l=1;l<=m;l++) b[l][ll] -= b[l][icol]*dum;
 			}
 	}
 	for (l=n;l>=1;l--) {
