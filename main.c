c91e27a660d682328e6f6015b28d039769a4edf9
Juliusz Gonera <juliusz.gonera@gmail.com>
2013-10-26 20:23:16 +0200

    Print out algorithm and whether omp was compiled in too

--- main.c
+++ main.c
@@ -29,12 +29,26 @@ double timediff(struct timeval start, struct timeval end) {
           (start.tv_sec * 1000 + start.tv_usec/1000));
 }
 
+void print_result(LinearEquation leq, struct timeval begin, const char *method) {
+  struct timeval end;
+  // record time
+  gettimeofday(&end, NULL);
+#ifdef OPENMP
+  printf("%.0f %.2f %s omp\n", timediff(begin, end),
+                               compute_error(leq), method);
+#else
+  printf("%.0f %.2f %s no_omp\n", timediff(begin, end),
+                                  compute_error(leq), method);
+#endif
+}
+
+
 void main (int argc, char **argv) {
   LinearEquation leq;
   Options opt = parse_command_line_arguments(argc, argv);
 
   // record time
-  struct timeval begin, end;
+  struct timeval begin;
   gettimeofday(&begin, NULL);
   int time_spent; // millis
 
@@ -42,19 +56,10 @@ void main (int argc, char **argv) {
 
   if (opt.use_conjugate_gradient) {
     run_linbcg(&leq, opt.tolerance, opt.max_iterations);
-
-    // record time
-    gettimeofday(&end, NULL);
-
-    // print out results
-    printf("%.0f %.2f\n", timediff(begin, end), compute_error(leq));
+    print_result(leq, begin,"conjugate_gradient");
   } else {
     run_gaussj(&leq);
-
-    // record time
-    gettimeofday(&end, NULL);
-
-    printf("%.0f %.2f\n", timediff(begin, end), compute_error(leq));
+    print_result(leq, begin,"gauss_jordan");
   }
 
   // free leq memory
